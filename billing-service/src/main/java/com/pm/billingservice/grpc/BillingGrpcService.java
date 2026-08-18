44d4f27f14ed874b5a6f33555895d5f3ea01e4f7
tejaskudva <tkudva@gmail.com>
2026-08-19 02:13:04 +0530

    Exposed 1 gRPC service as mock

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -1,9 +1,30 @@
 package com.pm.billingservice.grpc;
 
 import net.devh.boot.grpc.server.service.GrpcService;
+import billing.BillingResponse;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 
 @GrpcService
+@Slf4j
 public class BillingGrpcService extends BillingServiceImplBase {
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+            StreamObserver<billing.BillingResponse> responseObserver) {
+
+        log.info("createBillingAccount request received: {}", billingRequest);
+
+        // TODO business logic
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 
 }
